19d6eef20c40e730853e5263a63f83cf254e27fb
traffael <tschui@kth.se>
2016-06-30 15:36:20 +0200

    first commit: ADC readout and mainboard poking implemented and running on hardware

---
+++ state_machine.h
@@ -0,0 +1,49 @@
+/*
+ * state_machine.h
+ *
+ *  Created on: 09.05.2016
+ *      Author: beat
+ */
+
+#ifndef STATE_MACHINE_H_
+#define STATE_MACHINE_H_
+
+#include "eps_hal.h"
+#include <stdint.h>
+
+typedef struct _eps_status { //stores the answers to be sent to an eventual i2c request
+	uint16_t v_bat;
+	uint16_t t_bat;
+	uint16_t v_solar;
+	uint16_t current_in;
+	uint16_t current_out;
+	uint16_t analog_ext1;
+	uint16_t analog_ext2;
+	uint16_t analog_ext3;
+	uint16_t analog_ext4;
+
+	uint8_t v_bat_8;
+	uint8_t t_bat_8;
+	uint8_t v_solar_8;
+	uint8_t current_in_8;
+	uint8_t current_out_8;
+	uint8_t analog_ext1_8;
+	uint8_t analog_ext2_8;
+	uint8_t analog_ext3_8;
+	uint8_t analog_ext4_8;
+} eps_status_t;
+
+typedef enum _module_status{
+	MODULE_OFF=0,
+	TURN_OFF,
+	TURN_ON,
+	MODULE_ON,
+	FAULT
+} module_status_t;
+
+extern eps_status_t eps_status;
+extern module_status_t module_status[N_MODULES]; //stores the answers to be sent to an eventual i2c request
+
+
+
+#endif /* STATE_MACHINE_H_ */
